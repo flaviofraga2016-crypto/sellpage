301623c8a9f04dd66895279ebef3894815a3b4eb
flaviofraga2016-crypto <flaviofraga2016@gmail.com>
2026-03-10 16:41:29 -0300

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -2,14 +2,13 @@ import express from "express";
 import path from "path";
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
 const distPath = path.join(process.cwd(), "dist/public");
 
 app.use(express.static(distPath));
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(distPath, "index.html"));
 });
 

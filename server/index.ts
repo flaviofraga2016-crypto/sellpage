096406842f1a7ac8e271a50a56c3fae51c2ed5fb
flaviofraga2016-crypto <flaviofraga2016@gmail.com>
2026-03-28 10:22:11 -0300

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // pasta gerada pelo Vite
-const distPath = path.join(process.cwd(), "dist/public");
+const distPath = path.join(process.cwd(), "dist");
 
 // servir arquivos do frontend
 app.use(express.static(distPath));
